0b8889ea2028151fa39544b49db89e341cf668f7
datatree\hoya <didlsdydgh@gmail.com>
2025-01-03 16:08:56 +0900

    make button

--- lib/main.dart
+++ lib/main.dart
@@ -30,45 +30,49 @@ class MyHomePage extends StatefulWidget {
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
-//         title: const Text('Test Title'),
+//         title: const Text('Test App'),
 //       ),
 //       body: Container(),
 //     );
 //   }
 // }
 
-// 그라데이션 적용하기
+// 버튼 만들기기
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Test Title'),
+        title: const Text('Test App'),
       ),
       body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-            gradient: RadialGradient(
-                center: Alignment.center,
-                colors: [
-                  Colors.amber[100] as Color,
-                  Colors.amber[300] as Color,
-                  Colors.amber[500] as Color,
-                ],
-                stops: const [0.1, 0.5, 1.0],
-                radius: 1.0)
-            // gradient: LinearGradient(
-            //   colors: [
-            //     Colors.blue[100] as Color,
-            //     Colors.blue[300] as Color,
-            //     Colors.blue[500] as Color,
-            //   ],
-            //   begin: Alignment.topCenter,
-            //   end: Alignment.bottomCenter,
-            //   // stops: const [0.1, 0.4, 0.7],
-            // ),
+        child: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                    )),
+                onPressed: () => print('Elevated Button'),
+                child: const Text('Elevated Button'),
+              ),
             ),
+            Center(
+              child: TextButton(
+                onPressed: () => print('Text Button'),
+                child: const Text('Text Button'),
+              ),
+            ),
+            Center(
+              child: OutlinedButton(
+                onPressed: () => print('Outlined Button'),
+                child: const Text('Outlined Button'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -732,6 +736,43 @@ class _MyHomePageState extends State<MyHomePage> {
 //             ),
 //           ],
 //         ),
+//       ),
+//     );
+//   }
+// }
+
+// 그라데이션 적용하기
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Test Title'),
+//       ),
+//       body: Container(
+//         width: MediaQuery.of(context).size.width,
+//         height: MediaQuery.of(context).size.height,
+//         decoration: BoxDecoration(
+//             gradient: RadialGradient(
+//                 center: Alignment.center,
+//                 colors: [
+//                   Colors.amber[100] as Color,
+//                   Colors.amber[300] as Color,
+//                   Colors.amber[500] as Color,
+//                 ],
+//                 stops: const [0.1, 0.5, 1.0],
+//                 radius: 1.0)
+//             // gradient: LinearGradient(
+//             //   colors: [
+//             //     Colors.blue[100] as Color,
+//             //     Colors.blue[300] as Color,
+//             //     Colors.blue[500] as Color,
+//             //   ],
+//             //   begin: Alignment.topCenter,
+//             //   end: Alignment.bottomCenter,
+//             //   // stops: const [0.1, 0.4, 0.7],
+//             // ),
+//             ),
 //       ),
 //     );
 //   }
