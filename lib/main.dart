ca6a7de2203a5431cbfc903e392068a896027af6
JangKroed <didlsdydgh@gmail.com>
2025-01-04 22:41:53 +0900

    refresh indicator

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter App'),
-      ),
-      body: Container(),
-    );
+    return const Placeholder();
   }
 }
