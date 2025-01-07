7d10566263f5a4d1a0fb64177914ddcb8d66f4c8
JangKroed <didlsdydgh@gmail.com>
2025-01-08 00:45:05 +0900

    ListView Scroll Pagination

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,148 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'viewModel/albumViewModel.dart';
-import 'view/albumView.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ChangeNotifierProvider<AlbumViewModel>(
-        create: (context) => AlbumViewModel(),
-        child: const AlbumView(),
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _url = 'https://jsonplaceholder.typicode.com/albums';
+  int _page = 1;
+  final int _limit = 20;
+  bool _hasNextPage = true;
+  bool _isFirstLoadRunning = false;
+  bool _isLoadMoreRunning = false;
+  List _albumList = [];
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _initLoad();
+    _controller = ScrollController()..addListener(_nextLoad);
+  }
+
+  void _initLoad() async {
+    setState(() {
+      _isFirstLoadRunning = true;
+    });
+    try {
+      final res =
+          await http.get(Uri.parse('$_url?_page=$_page&_limit=$_limit'));
+      setState(() {
+        _albumList = jsonDecode(res.body);
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+
+    setState(() {
+      _isFirstLoadRunning = false;
+    });
+  }
+
+  void _nextLoad() async {
+    if (_hasNextPage &&
+        !_isFirstLoadRunning &&
+        !_isLoadMoreRunning &&
+        _controller.position.extentAfter < 100) {
+      setState(() {
+        _isLoadMoreRunning = true;
+      });
+      _page += 1;
+      try {
+        final res =
+            await http.get(Uri.parse('$_url?_page=$_page&_limit=$_limit'));
+        final List fetchedAlbums = json.decode(res.body);
+        if (fetchedAlbums.isNotEmpty) {
+          setState(() {
+            _albumList.addAll(fetchedAlbums);
+          });
+        } else {
+          setState(() {
+            _hasNextPage = false;
+          });
+        }
+      } catch (e) {
+        print(e.toString());
+      }
+
+      setState(() {
+        _isLoadMoreRunning = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_nextLoad);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Pagination'),
       ),
+      body: _isFirstLoadRunning
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    controller: _controller,
+                    itemCount: _albumList.length,
+                    itemBuilder: (context, index) => Card(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 10),
+                      child: ListTile(
+                        title: Text(_albumList[index]['id'].toString()),
+                        subtitle: Text(_albumList[index]['title']),
+                      ),
+                    ),
+                  ),
+                ),
+                if (_isLoadMoreRunning)
+                  Container(
+                    padding: const EdgeInsets.all(30),
+                    child: const Center(child: CircularProgressIndicator()),
+                  ),
+                if (!_hasNextPage)
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.blue,
+                    child: const Center(
+                      child: Text(
+                        'No more data to be fetched',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+              ],
+            ),
     );
   }
 }
